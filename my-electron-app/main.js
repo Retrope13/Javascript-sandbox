97a585b2ba840fe454986e5434aad653ecbdadb6
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2024-01-10 23:05:47 -0700

    idea to include react type functions

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,6 +1,23 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
+const { createHash } = require('node:crypto');
 
 const path = require('node:path')
+
+//*Look into making js functions return the html. Then you can actually use fancy react stuff
+function Home() {
+    return (
+        <body>
+            <h1>Hello World!</h1>
+            We are using Node.js <span id="node-version"></span>,
+            Chromium <span id="chrome-version"></span>,
+            and Electron <span id="electron-version"></span>.
+
+            <script src="./renderer.js"></script>
+        </body>
+
+    );
+
+}
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -18,7 +35,7 @@ const createWindow = () => {
 //When electron has finished initialization and is ready to create browser windows. Some APIs can only be used after this event occurs
 app.whenReady().then(() => {
     createWindow()
-
+    createHash()
     app.on('activate', () => {
         //for macOS this re-creates a window in the app when the dock icon is clicked and there are no other windows open
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
